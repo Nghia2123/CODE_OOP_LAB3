2c044b883f4eb4d4c73dca8cb9fe98c7394660dd
Nghia2123 <155422593+Nghia2123@users.noreply.github.com>
2024-11-10 17:33:18 +0700

    Add files via upload

--- Bai3/main.cpp
+++ Bai3/main.cpp
@@ -3,16 +3,34 @@
 using namespace std;
 
 int main() {
-	ThoiGian A;
+	ThoiGian A, B;
 	int h, m, s, n;
 	cout << "Nhap thoi gian A (gio phut giay): ";
 	cin >> A;
 	cout << "Nhap thoi gian B (gio phut giay): ";
-	cin >> h >> m >> s;
-	ThoiGian B(h, m, s);
+	do {
+		cin >> h >> m >> s;
+		if (h < 0 || h > 23 || m < 0 
+			|| m > 59 || s < 0 || s > 59) {
+			cout << "Thoi gian khong hop le, nhap lai: ";
+			continue;
+		}
+		else {
+			B = ThoiGian(h, m, s);
+			break;
+		}
+	} while (true);
 
 	cout << "Nhap so giay n: ";
-	cin >> n;
+	do {
+		cin >> n;
+		if (n < 0) {
+			cout << "So giay khong hop le, nhap lai: ";
+			continue;
+		}
+		else
+			break;
+	} while (true);
 
 	cout << "A sang giay: " << A.TinhGiay() << endl;
 	ThoiGian C;
